930e7c6e9c85d65a2290990293af97c5c7d7f78e
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-10-27 10:38:12 -0400

    import BCrypt

--- src/main/java/com/example/techtalenttwitter/twitter/configuration/WebMvcConfiguration.java
+++ src/main/java/com/example/techtalenttwitter/twitter/configuration/WebMvcConfiguration.java
@@ -2,6 +2,7 @@ package com.example.techtalenttwitter.twitter.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
