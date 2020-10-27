ec1944da19f54f66dddb5dbd47508314e7918c18
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-10-27 11:27:53 -0400

    Fill from TTS slides

--- src/main/java/com/example/techtalenttwitter/twitter/repository/TweetRepository.java
+++ src/main/java/com/example/techtalenttwitter/twitter/repository/TweetRepository.java
@@ -1,5 +1,16 @@
 package com.example.techtalenttwitter.twitter.repository;
 
-public class TweetRepository {
-    
+import java.util.List;
+
+import com.example.techtalenttwitter.twitter.model.Tweet;
+import com.example.techtalenttwitter.twitter.model.User;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TweetRepository extends CrudRepository<Tweet, Long> {
+    List<Tweet> findAllByOrderByCreatedAtDesc();
+    List<Tweet> findAllByUserOrderByCreatedAtDesc(User user);
+    List<Tweet> findAllByUserInOrderByCreatedAtDesc(List<User> users);
 }
