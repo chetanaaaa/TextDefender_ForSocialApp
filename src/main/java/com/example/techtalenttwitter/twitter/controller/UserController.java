7afcbded1654163848d77dee50be1f7bd0032c13
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-10-28 09:51:28 -0400

    init from TTS slides, add SetTweetCounts method

--- src/main/java/com/example/techtalenttwitter/twitter/controller/UserController.java
+++ src/main/java/com/example/techtalenttwitter/twitter/controller/UserController.java
@@ -1,5 +1,41 @@
 package com.example.techtalenttwitter.twitter.controller;
 
+import java.util.HashMap;
+import java.util.List;
+
+import com.example.techtalenttwitter.twitter.model.Tweet;
+import com.example.techtalenttwitter.twitter.model.User;
+import com.example.techtalenttwitter.twitter.service.TweetService;
+import com.example.techtalenttwitter.twitter.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
-    
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TweetService tweetService;
+
+    @GetMapping(value = "/users")
+    public String getUsers(Model model) {
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
+        SetTweetCounts(users, model);
+        return "users";
+    }
+
+    private void SetTweetCounts(List<User> users, Model model) {
+        HashMap<String,Integer> tweetCounts = new HashMap<>();
+        for (User user : users) {
+            List<Tweet> tweets = tweetService.findAllByUser(user);
+            tweetCounts.put(user.getUsername(), tweets.size());
+        }
+        model.addAttribute("tweetCounts", tweetCounts);
+    }
 }
