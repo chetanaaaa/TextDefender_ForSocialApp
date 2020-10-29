eccddaa058e298562911d39b8db28d8b34c566ef
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-10-29 09:26:46 -0400

    init from TTS slides

---
+++ src/main/java/com/example/techtalenttwitter/twitter/model/Tag.java
@@ -0,0 +1,68 @@
+package com.example.techtalenttwitter.twitter.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "tag_id")
+    private Long id;
+
+    private String phrase;
+
+    @ManyToMany(mappedBy = "tags")
+    private List<Tweet> tweets;
+
+
+    public Tag() {
+    }
+
+
+    public Tag(Long id, String phrase, List<Tweet> tweets) {
+        this.id = id;
+        this.phrase = phrase;
+        this.tweets = tweets;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPhrase() {
+        return this.phrase;
+    }
+
+    public void setPhrase(String phrase) {
+        this.phrase = phrase;
+    }
+
+    public List<Tweet> getTweets() {
+        return this.tweets;
+    }
+
+    public void setTweets(List<Tweet> tweets) {
+        this.tweets = tweets;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", phrase='" + getPhrase() + "'" +
+            ", tweets='" + getTweets() + "'" +
+            "}";
+    }
+}
